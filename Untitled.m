152aa8e2fe27c062202d4daa1eeb446bcc37c68c
yeoleparesh <yeoleparesh@gmail.com>
2017-03-22 22:35:32 +0530

    Add files via upload

--- Untitled.m
+++ Untitled.m
@@ -1,47 +1,94 @@
 clc; clear all; close all;
 %
-vl=1;
-tfinal=200;
-f=1;
+vl=1.3;
+tfinal=100;
+%f=1;
 %
 i=1;
-vf(i)=1;
-
+vf(i)=0;
 t(i)=0;
-xl(i)=0;
-yl(i)=0;
-thtl(i)=0;
-xf(i)=2;
-yf(i)=2;
-thtf(i)=0;
-a(i)=sin(f*t(i));
+f=0.55;
+wl(i)=-sin(f*t(i));
+%wl(i)=6;
+xl(i)=5;
+yl(i)=2;
+thtl(i)=pi/2;
+xf(i)=3.5;
+yf(i)=2.2;
+thtf(i)=pi/6;
+%a(i)=sin(f*t(i));
+
 %
-phi(i)=atan((yl(i)-yf(i))/((xl(i)-xf(i))))-thtf(i);
-d(i)=sqrt((yl(i)-yf(i))^2+((xl(i)-xf(i)))^2);
+%phi(i)=atan((yl(i)-yf(i))/((xl(i)-xf(i))))-thtf(i);
+%d(i)=sqrt((yl(i)-yf(i))^2+((xl(i)-xf(i)))^2);
+phi=-pi/3;
+d=0.7;
 %
 h=0.01;
 %
+wfmax=pi;
+K=0.7692;
+vlmax=1.5;
+vfmax=4;
+
 while t(i)<tfinal
     %Leader
+      
     xl(i+1) = xl(i) + h*vl*cos(thtl(i));
     yl(i+1) = yl(i) + h*vl*sin(thtl(i));
-    thtl(i+1) = thtl(i) + h*a(i)/vl;
+    thtl(i+1) = thtl(i) + h*wl(i);
     %Follower
+    
+    %
+   
+    %
+    %phi(i)=atan((yl(i)-yf(i))/((xl(i)-xf(i))))-thtf(i);
+    %d(i)=sqrt((yl(i)-yf(i))^2+((xl(i)-xf(i)))^2);
+    %vf(i)=(vl*cos(thtl(i)-thtf(i)-phi)/cos(phi));
+    %
+    %a(i)=sin(f*t(i));
+    %a(i)=0.4;
+   
+    beta(i) = thtl(i)-thtf(i);
+    alpha=asin(K*d*cos(phi));
+    if beta(i)>0
+        if beta(i)>alpha
+            vf(i)=vfmax;
+            wf(i)=wfmax;
+        else 
+            vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
+            if vf(i)>vfmax
+                vf(i)=vfmax;
+            end
+            wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
+        end
+    else
+        if beta(i) < alpha
+            vf(i)=vfmax;
+            wf(i)=-wfmax;
+        else 
+            vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
+            if vf(i)>vfmax
+                vf(i)=vfmax;
+            end
+            wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
+        end
+    end
+     %
     xf(i+1) = xf(i) + h*vf(i)*cos(thtf(i));
     yf(i+1) = yf(i) + h*vf(i)*sin(thtf(i));
-    thtf(i+1)= thtf(i) + ((h*vf(i)*sin(thtl(i)-thtf(i)))/((d(i)*cos(phi(i)))));  
+    thtf(i+1)= thtf(i) + h*wf(i);  
     %
     i=i+1;
     t(i)=t(i-1)+h;
-    %
-    phi(i)=atan((yl(i)-yf(i))/((xl(i)-xf(i))))-thtf(i);
-    d(i)=sqrt((yl(i)-yf(i))^2+((xl(i)-xf(i)))^2);
-    vf(i)=(vl*cos(thtl(i)-thtf(i)-phi(i))/cos(phi(i)));
-    %
-    a(i)=sin(f*t(i));
+    wl(i)=-sin(f*t(i));
+    %wl(i)=6;
 end
 %
 plot(xl,yl,'r',xf,yf); axis equal; grid on;
+%figure; plot(t,a); grid on;
+figure; plot(t,thtf); grid on;
+figure; plot(t,thtl); grid on;
 %figure; plot(t, a); grid on;
 %figure; plot(t, tht); grid on;
 %
