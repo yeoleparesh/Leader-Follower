24b685a68692092488802aa5e58b020116ae51dc
yeoleparesh <yeoleparesh@gmail.com>
2017-03-23 21:39:36 +0530

    Add files via upload

--- Untitled.m
+++ Untitled.m
@@ -8,32 +8,58 @@ i=1;
 vf(i)=0;
 t(i)=0;
 f=0.55;
-wl(i)=-sin(f*t(i));
+wl(i)=-sin(1.1*t(i));
 %wl(i)=6;
-xl(i)=5;
-yl(i)=2;
+xl(i)=1/sqrt(2);
+yl(i)=1/sqrt(2);
 thtl(i)=pi/2;
-xf(i)=3.5;
-yf(i)=2.2;
-thtf(i)=pi/6;
+xf(i)=0;
+yf(i)=0;
+thtf(i)=pi/8;
 %a(i)=sin(f*t(i));
 
 %
 %phi(i)=atan((yl(i)-yf(i))/((xl(i)-xf(i))))-thtf(i);
 %d(i)=sqrt((yl(i)-yf(i))^2+((xl(i)-xf(i)))^2);
 phi=-pi/3;
-d=0.7;
+d=1;
 %
 h=0.01;
 %
 wfmax=pi;
 K=0.7692;
-vlmax=1.5;
+Kpp=3;
+Kp=-2;
+vlmax=2;
 vfmax=4;
+%
+%
+bta(i) = thtl(i)-thtf(i);
+%alpha=asin(K*d*cos(phi));
+%while (beta(i)>=0 && beta(i)>alpha) || (beta(i)<0 && beta(i)<alpha)
+%    vf(i)=0;
+%    if beta(i)>alpha
+%        wf(i)=wfmax;
+%    else
+%        wf(i)=-wfmax;
+%    end
+%end
+
+    
+%
+%
 
 while t(i)<tfinal
     %Leader
-      
+%     Koo=0.5;
+%     Ko=-1;
+% 
+%     alpha1=asin(Koo*d*cos(phi));
+%     alpha2=asin(Ko*d*cos(phi));
+%     while beta(i)>alpha1 && beta(i)<alpha2
+%         vf(i)=
+%     end
+
     xl(i+1) = xl(i) + h*vl*cos(thtl(i));
     yl(i+1) = yl(i) + h*vl*sin(thtl(i));
     thtl(i+1) = thtl(i) + h*wl(i);
@@ -49,39 +75,45 @@ while t(i)<tfinal
     %a(i)=sin(f*t(i));
     %a(i)=0.4;
    
-    beta(i) = thtl(i)-thtf(i);
-    alpha=asin(K*d*cos(phi));
-    if beta(i)>0
-        if beta(i)>alpha
-            vf(i)=vfmax;
-            wf(i)=wfmax;
-        else 
-            vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
-            if vf(i)>vfmax
-                vf(i)=vfmax;
-            end
-            wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
-        end
-    else
-        if beta(i) < alpha
-            vf(i)=vfmax;
-            wf(i)=-wfmax;
-        else 
-            vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
-            if vf(i)>vfmax
-                vf(i)=vfmax;
-            end
-            wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
-        end
-    end
-     %
+    %beta(i) = thtl(i)-thtf(i);
+    %alpha=asin(Kpp*d*cos(phi));
+    
+    
+    %
+%     if beta(i)>=0
+%         if beta(i)>alpha
+%             vf(i)=vfmax;
+%             wf(i)=vfmax*Kpp;
+%         else 
+%             vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
+%             if vf(i)>vfmax
+%                 vf(i)=vfmax;
+%             end
+%             wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
+%         end
+%     else
+%         if beta(i) < alpha
+%             vf(i)=vfmax;
+%             wf(i)=vfmax*Kp;
+%         else 
+%             vf(i)=(vl*cos(beta(i)-phi))/(cos(phi));
+%             if vf(i)>vfmax
+%                 vf(i)=vfmax;
+%             end
+%             wf(i)=(vl*sin(beta(i)))/(d*cos(phi));
+%         end
+%     end
+%      %
+     vf(i)=(vl*cos(bta(i)-phi))/(cos(phi));
+     wf(i)=(vl*sin(bta(i)))/(d*cos(phi));
     xf(i+1) = xf(i) + h*vf(i)*cos(thtf(i));
     yf(i+1) = yf(i) + h*vf(i)*sin(thtf(i));
     thtf(i+1)= thtf(i) + h*wf(i);  
     %
     i=i+1;
     t(i)=t(i-1)+h;
-    wl(i)=-sin(f*t(i));
+    wl(i)=-sin(1.1*t(i));
+    bta(i)=thtl(i)-thtf(i);
     %wl(i)=6;
 end
 %
