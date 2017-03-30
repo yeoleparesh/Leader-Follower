df59ceb6c015f65e97f1a9c54974eca55adbc0a2
yeoleparesh <yeoleparesh@gmail.com>
2017-03-30 20:37:36 +0530

    Add files via upload

--- Alternative_v1_1.m
+++ Alternative_v1_1.m
@@ -1,7 +1,7 @@
 clc; clear all; close all;
 %
 vl=1.3;
-tfinal=10;
+tfinal=20;
 %
 i=1;
 vf(i)=0;
@@ -15,7 +15,7 @@ thtl(i)=pi/2;
 xf(i)=4;
 yf(i)=2;
 thtf(i)=pi/8;
-phi=-pi/6;
+phi=+pi/6;
 d=1;
 %
 h=0.001;
@@ -26,35 +26,35 @@ vlmax=2;
 vfmax=4;
 %
 %
-bta(i) = thtl(i)-thtf(i);
 
-    
+l(i)=0;
+etan(i)=0;
+alphan(i)=0;
+btan(i)=0;
+wf(i)=0;    
 %
 %
 
 while t(i)<tfinal
     %Leader
         %disp('1')
+        %Follower
+        l(i)=sqrt(((xl(i)-(xf(i)+0.2*cos(thtf(i))))^2)+((yl(i)-(yf(i)+0.2*sin(thtf(i))))^2));
+        etan(i)=atan2(yl(i)-(yf(i)+0.2*sin(thtf(i))),xl(i)-(xf(i)+0.2*cos(thtf(i))));
+        alphan(i)=etan(i)-thtl(i);
+        btan(i)=etan(i)-thtf(i);
+        vf(i)=-(d-l(i))*cos(btan(i))+(phi-alphan(i))*l(i)*cos(btan(i))-l(i)*sin(btan(i))*wl(i)-vl*cos(alphan(i)+btan(i));
+        wf(i)=(-(d-l(i))*sin(btan(i))-(phi-alphan(i))*l(i)*cos(btan(i))+l(i)*cos(btan(i))*wl(i)+vl*sin(-alphan(i)+btan(i)))/0.2;
         xl(i+1) = xl(i) + h*vl*cos(thtl(i));
         yl(i+1) = yl(i) + h*vl*sin(thtl(i));
         thtl(i+1) = thtl(i) + h*wl(i);
-        %Follower
-        l(i)=sqrt(((xl(i)-xf(i))^2)+((yl(i)-yf(i))^2));
-        etan(i)=atan2(yl(i)-yf(i),xl(i)-xf(i));
-        alphan(i)=etan(i)-thtl(i);
-        btan(i)=etan(i)-thtf(i);
-        vf(i)=-d*cos(btan(i))+l(i)*cos(btan(i))+l(i)*phi*sin(btan(i))-alphan(i)*l(i)*sin(btan(i))-l(i)*sin(btan(i))*wl(i)+vl*cos(alphan(i)-btan(i));
-        wf(i)=-sin(btan(i))-l(i)*sin(btan(i))-l(i)*phi*cos(btan(i))+l(i)*cos(btan(i))*alphan(i)+l(i)*cos(btan(i))*wl(i)-vl*sin(alphan(i)+btan(i));
         xf(i+1) = xf(i) + h*vf(i)*cos(thtf(i));
         yf(i+1) = yf(i) + h*vf(i)*sin(thtf(i));
-        thtf(i+1)= thtf(i) + h*wf(i);  
-    
-   
-    i=i+1;
-    t(i)=t(i-1)+h;
-    wl(i)=-sin(1.1*t(i));
-    
-    
+        thtf(i+1)= thtf(i) + h*wf(i);    
+        i=i+1;
+        t(i)=t(i-1)+h;
+        wl(i)=-sin(1.1*t(i));
+       
 end
 %
 plot(xl,yl,'r',xf,yf); axis equal; grid on;
